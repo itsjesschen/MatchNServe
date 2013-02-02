ad095f8405a82f76615a81a4e9c95029d3ad01e0
Rodrigo Santos <rigotek@gmail.com>
2013-02-02 14:04:24 -0800

    added header, footer, home page

---
+++ MatchServe/application/views/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- header containing all the bootstrap calls -->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>FooterPage</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+    
+    <style type="text/css">
+
+	  
+    </style>
+</head>
+
+
+
+<body>
+	<div class="footer">
+	<div class="navbar navbar-inverse">
+  		<div class="navbar-inner">
+    		<ul class="nav">
+      			<li><a href="#">The Team</a></li>
+      			<li><a href="#">Press</a></li>
+                <li><a href="#">Careers</a></li>
+      			<li><a href="#">Donate</a></li>
+                <li><a href="#">Clients</a></li>
+      			<li><a href="#">Learning Center</a></li>
+    		</ul>
+  		</div>
+	</div>
+	</div>
+</body>
+</html>
