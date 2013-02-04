b780ea6294fd769367035595646b9091c214bbb1
unknown <cprynn@gmail.com>
2013-02-03 21:52:46 -0800

    refs #24 Added login.php to views, login.php to controllers, facebooklogin.php to controllers, and accountselection.php to controllers.

---
+++ MatchServe/application/controllers/accountselection.php
@@ -0,0 +1,46 @@
+<?php
+$dbLocalhost = mysql_connect("localhost", "root", "")
+or die("Could not connect: " . mysql_error());
+mysql_select_db("matchserve", $dbLocalhost)
+or die("Could not find database: " . mysql_error());
+    if (isset($_POST['userName']))
+	{
+		$userName = $_POST['userName'];
+		$results = mysql_query("SELECT organizations.Name FROM organizations, admins, users WHERE (users.Name='$userName' AND admins.UserID=users.UserID AND organizations.OrganizationID=admins.OrganizationID)");
+			$text = "<html>";
+			$text = $text . "<style type='text/css'>";
+			$text = $text . ".button {";
+			$text = $text . "background-color:grey;padding:15px;padding-right:20px;padding-left:20px;";
+			$text = $text . "}";
+			$text = $text . "</style>";
+			$text = $text . "<form id='myform' action='http://www.matchandserve.com' method='post'>";
+			$text = $text . "<table cellspacing='10px'>";
+			$count = 0;
+			$text = $text . "<tr><td class='button' onclick=\"clicked('personal')\" align='center'>Personal Account</td></tr>";
+			while($row = mysql_fetch_assoc($results)) 
+			{
+				$name = $row['Name'];
+				$text = $text . "<tr><td class='button' onclick=\"clicked('$name')\" align='center'>$name</td></tr>";
+				$count++;
+			}
+			if ($count > 0)
+			{
+				echo $text;
+				echo "<tr><td><input type='hidden' name='account' id='account' value=''/></td></tr>";
+				echo "<tr><td><input type='hidden' name='name' value='$name'/></td></tr>";
+				echo "</table>";
+				echo "</form>";
+				echo "<script type='text/javascript'>";
+				echo "function clicked(name) {";
+				echo "document.getElementById('account').value = name;";
+				echo "document.getElementById('myform').submit();";
+				echo "}";
+				echo "</script>";
+				echo "</html>";
+			}
+			else
+			{
+				header('Location: http://www.matchandserve.com');
+			}
+		}
+?>
--- MatchServe/application/controllers/login.php
+++ MatchServe/application/controllers/login.php
@@ -4,7 +4,7 @@ function generateRandomString($length = 8) {
 }
 $dbLocalhost = mysql_connect("localhost", "root", "")
 or die("Could not connect: " . mysql_error());
-mysql_select_db("database1", $dbLocalhost)
+mysql_select_db("matchserve", $dbLocalhost)
 or die("Could not find database: " . mysql_error());
 
     if (isset($_POST['submit']))
@@ -16,27 +16,43 @@ or die("Could not find database: " . mysql_error());
 		{
 			if ($password != null && $password !="") 
 			{
-				$result = mysql_query("SELECT Password FROM users WHERE Name='$userName'", $dbLocalhost)
-				or die("Your record does not exist." . mysql_error());
-				while($row = mysql_fetch_array($result))
-				  {
-					if ($_POST['password'] == $row['Password'])
-					{
-					//	cookie::put('name', '$userName', 7200);
-						echo "Successful Login";
-					}
-					else
-					{
-						echo "Incorrect password!<a href='login.html'>Forgot Password</a>";
-					}
-				  }
+				$result = mysql_query("SELECT Password FROM users WHERE Name='$userName'", $dbLocalhost);
+				$count = 0;
+					while($row = mysql_fetch_array($result))
+					  {
+					  $count++;
+						if ($_POST['password'] == $row['Password'])
+						{
+						//	cookie::put('name', '$userName', 7200);
+							echo "Successful Login";
+							echo "<html>";
+							echo "<form id='myform' action='accountselection.php' method='post'>";
+							echo "<input type='hidden' name='userName' value='$userName' />";
+							echo "</form>";
+							echo "<script type='text/javascript'>";
+							echo "document.getElementById('myform').submit();";
+							echo "</script>";
+							echo "</html>";
+						}
+						else
+						{
+							echo "Incorrect password! <a href='../views/login.php'>Forgot Password</a>";
+							$count = 2;
+						}
+					  }
+					  if($count <= 1)
+					  {
+						echo "Your record does not exist. <a href='../views/login.php'>Try again</a>";
+					  }
 			}
 			else 
 			{
 				$result = mysql_query("SELECT Email FROM users WHERE Name='$userName'", $dbLocalhost)
 					or die("Problem reading table: " . mysql_error());
+					$count = 0;
 				while($row = mysql_fetch_array($result))
 				{
+					$count++;
 					$temp=generateRandomString();
 					$email = $row['Email'];
 					// The message
@@ -49,20 +65,31 @@ or die("Could not find database: " . mysql_error());
 					mail('$email', 'Forgot Password', $message);
 					echo "Email sent";
 				}
+				 if($count <= 0)
+			     {
+					echo "Your record does not exist. <a href='../views/login.php'>Try again</a>";
+				 }
 			}
 		}
 		else if ($newPassword!=null && $newPassword !="")
 		{
 			$email = $_POST['newEmail'];
-			$temp1 = mysql_query("SELECT Password FROM users WHERE Name='$userName'", $dbLocalhost)
-			if ($temp1 == null) 
+			$temp1 = mysql_query("SELECT * FROM users WHERE Name='$userName'", $dbLocalhost);
+			$count = 0;
+			while($row = mysql_fetch_assoc($temp1)) 
+			{
+				$count++;
+			}
+			
+			if ($count <= 0) 
 			{
 				if ($password == $newPassword)
 				{
-					$sql="insert into users(Name, Email, Password) values('".$userName"','".$email."','".$newPassword."')";
+					$sql="insert into users(Name, Email, Password) values('$userName','$email','$newPassword')";
 					$result = mysql_query($sql, $dbLocalhost)
 					or die("Problem writing to table: " . mysql_error());
 					echo "Successfully created new user";
+					header('Location: http://www.matchandserve.com');
 				}
 				else
 				{
