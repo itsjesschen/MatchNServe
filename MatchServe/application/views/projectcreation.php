260c5877281ba062eb2a0b2b9ecdcc051b1f04b0
Rodrigo Santos <rigotek@gmail.com>
2013-02-26 09:45:05 -0800

    just made the full project description a bit bigger, however i am having trouble vertically aligning the text to start at the top rather than remain in the center

--- MatchServe/application/views/projectcreation.php
+++ MatchServe/application/views/projectcreation.php
@@ -2,28 +2,28 @@
 <html lang="en">
 <head>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" charset="utf-8">
-    <title>Match and Serve - Create A Project</title>
-    <meta name="viewport" content="width=device-width">
-    <?php echo Asset::container('bootstrap')->styles();?>
-  	<?php echo Asset::scripts();?>
-    <?php echo Asset::container('projectcreation')->scripts();?>
-    <?php echo Asset::container('datepicker')->scripts();?>
-    <?php echo Asset::styles();?> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" charset="utf-8">
+	<title>Match and Serve - Create A Project</title>
+	<meta name="viewport" content="width=device-width">
+	<?php echo Asset::container('bootstrap')->styles();?>
+	<?php echo Asset::scripts();?>
+	<?php echo Asset::container('projectcreation')->scripts();?>
+	<?php echo Asset::container('datepicker')->scripts();?>
+	<?php echo Asset::styles();?> 
 
 
-    <style type="text/css">
+	<style type="text/css">
 
-    a.dp-choose-date {
-	float: left;
-	width: 16px;
-	height: 16px;
-	padding: 0;
-	margin: 5px 3px 0;
-	display: block;
-	text-indent: -2000px;
-	overflow: hidden;
-	background: url(calendar.png) no-repeat; 
+	a.dp-choose-date {
+		float: left;
+		width: 16px;
+		height: 16px;
+		padding: 0;
+		margin: 5px 3px 0;
+		display: block;
+		text-indent: -2000px;
+		overflow: hidden;
+		background: url(calendar.png) no-repeat; 
 	}
 
 	a.dp-choose-date.dp-disabled {
@@ -31,7 +31,14 @@
 		cursor: default;
 	}
 
-    </style>
+	#basicInfoGetter{
+		margin-left: 20px;
+	}
+	#projectDescription{
+		height:150px;
+	}
+
+	</style>
 </head>
 
 <body>
@@ -44,36 +51,37 @@
 	<?php echo '<p> Hello. You are using:</p>';
 	echo $_SERVER['HTTP_USER_AGENT']; ?>
 -->
-	<div class="dashboard"></div>
-	<div class="subDashboard"></div>
-	</br>
-	<form action="projectcreation/test" method="get">
+<div class="dashboard"></div>
+<div class="subDashboard"></div>
+</br>
+<div class="workspace">
+	<form action="projectcreation/test" method="get" id="basicInfoGetter">
 
-        <div id="projectcreation-specifiers-container">
-	 		<input id="projectName" type="text" width="100" name="projectName" value="Give your project a name" defaultValue = "Give your project a name" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
-            </br></br>
-	 		<input id="projectHeadline" type="text" width="100" name="projectHeadline" value="What's the jist?" defaultValue = "What's the jist?" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
-            </br></br>
-	 		<input id="projectDescription" type="text" width="100" name="projectDescription" value="Full project description" defaultValue = "Full project description" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
-            </br></br>
-	 		<input id="projectDate" type="text" width="100" name="projectDate" value="Choose project date" defaultValue = "Choose project date" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
-            </br></br>
+		<div id="projectcreation-specifiers-container">
+			<input id="projectName" type="text" width="100" name="projectName" value="Give your project a name" defaultValue = "Give your project a name" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
+		</br></br>
+		<input id="projectHeadline" type="text" width="100" name="projectHeadline" value="What's the jist?" defaultValue = "What's the jist?" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
+	</br></br>
+	<input id="projectDescription" type="text" width="100" name="projectDescription" value="Full project description" defaultValue = "Full project description" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
+</br></br>
+<input id="projectDate" type="text" width="100" name="projectDate" value="Choose project date" defaultValue = "Choose project date" onclick="searchFieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"/>
+</br></br>
 
-			<select class="projectAdminSelector">
-				<option disabled> Select an admin </option>
-			</select>
-
-
-
-		</div>
-
- 		<p><p><input type="submit" /></p></p>
-	</form>
+<select class="projectAdminSelector">
+	<option disabled> Select an admin </option>
+</select>
 
 
-	<div class="footer">
-		<?php echo render('elements.footer'); ?>
-	</div>
+
+</div>
+
+<p><p><input type="submit" /></p></p>
+</form>
+</div>
+
+<div class="footer">
+	<?php echo render('elements.footer'); ?>
+</div>
 
 </body>
 </html>
