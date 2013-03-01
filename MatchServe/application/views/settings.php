c481fa08bd1b6d44173a68da29219fe5419a0cd2
unknown <cprynn@gmail.com>
2013-03-01 00:13:56 -0800

    refs #41 finished settings

--- MatchServe/application/views/settings.php
+++ MatchServe/application/views/settings.php
@@ -99,28 +99,49 @@
 	font-size:20px;
 	font-weight:bold;
 	}
-	td {
-	padding-right:200px;
+	.labelText {
+		font-weight: bold;
+		width:200px;
 	}
+	.btnColumn {
+		align:right;
+	}
+table {
+  max-width: 100%;
+  background-color: transparent;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
 
+.table1 {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.table1 td {
+  padding: 8px;
+  line-height: 20px;
+  text-align: left;
+  vertical-align: top;
+}
      </style>
 	 
 	 <script language="javascript">		
 			function editUsername() {
 				username = document.getElementById('username').innerHTML;
 				document.getElementById('username').innerHTML = "<input type='text' class='formElementSpacing' name='username' value='" + username + "'>";
-				document.getElementById('usernameLink').innerHTML = "<input type='submit' name='save' value='Save'>";
+				document.getElementById('usernameLink').innerHTML = "<input type='submit' class='btn' name='save' value='Save'>";
 			}
 			
 			function editEmail() {
 				email = document.getElementById('email').innerHTML;
 				document.getElementById('email').innerHTML = "<input type='text' class='formElementSpacing' name='email' value='"+ email + "'>";
-				document.getElementById('emailLink').innerHTML = "<input type='submit' name='save' value='Save'>";
+				document.getElementById('emailLink').innerHTML = "<input type='submit' class='btn' name='save' value='Save'>";
 			}
 			
 			function editPassword() {
 				document.getElementById('password').innerHTML = "<input type='password' class='formElementSpacing' name='password'>";
-				document.getElementById('passwordLink').innerHTML = "<input type='submit' name='save' value='Save'>";
+				document.getElementById('passwordLink').innerHTML = "<input type='submit' class='btn' name='save' value='Save'>";
 			}
 			
 			function remove(name){
@@ -133,6 +154,7 @@
 			function addadmin() {
 				document.getElementById('admintoadd').style.visibility = "visible";
 				document.getElementById('add').style.visibility = "visible";
+				document.getElementById('empty').style.visibility = "visible";
 			}
 			
 			function add() {
@@ -147,7 +169,7 @@
     
     <form id="settingsForm" action= <?php echo URL::to('settings/savesettings'); ?> method="post"> 
 		<br><br><h1>Personal Account Settings</h1>
-			<table class="table-striped">
+			<table class="table">
 				<?php 
 					$data = Session::get('data');
 					$data = json_decode($data);
@@ -161,26 +183,26 @@
 							case 0:
 								$password = $obj->password;
 								$email = $obj->email;
-								echo "<tr><td>Username</td><td><div id='username'>".$name."</div></td><td><div id='usernameLink'><a class='link' href='javascript:editUsername()'>Edit</a></div></td></tr>";
-								echo "<tr><td>Email</td><td><div id='email'>".$email."</div></td><td><div id='emailLink'><a class='link' href='javascript:editEmail()'>Edit</a></div></td></tr>";
-								echo "<tr><td>Password</td><td><div id='password'></div></td><td><div id='passwordLink'><a class='link' href='javascript:editPassword()'>Edit</a></div></td></tr>";
-								echo "</table>";
+								echo "<tr><td class='labelText'>Username</td><td><div id='username'>".$name."</div></td><td><div align='right' id='usernameLink'><a class='link' href='javascript:editUsername()'>Edit</a></div></td></tr>";
+								echo "<tr><td class='labelText'>Email</td><td><div id='email'>".$email."</div></td><td><div align='right' id='emailLink'><a class='link' href='javascript:editEmail()'>Edit</a></div></td></tr>";
+								echo "<tr><td class='labelText'>Password</td><td><div id='password'></div></td><td><div align='right' id='passwordLink'><a class='link' href='javascript:editPassword()'>Edit</a></div></td></tr>";
+								echo "<tr><td></td><td></td><td></td></tr></table>";
 								break;
 							case 1:
 								echo "<h1>".$account ." Account Settings</h1>";
-								echo "<table class='table-striped'><tr><td>Administrators</td><td>".$name."</td><td><a class='link' href='javascript:void(0)' onclick=\"remove('".$name."')\">Remove</a></td></tr>";
+								echo "<table class='table1'><tr class='table'><td class='labelText'>Administrators</td><td>".$name."</td><td><div align='right'><a class='link' href='javascript:void(0)' onclick=\"remove('".$name."')\">Remove</a></div></td></tr>";
 								break;
 							default:
-								echo "<tr><td></td><td>".$name."</td><td><a class='link' href='javascript:void(0)' onclick=\"remove('".$name."')\">Remove</a></td></tr>";
+								echo "<tr><td></td><td>".$name."</td><td class='btnColumn'><div align='right'><a class='link' href='javascript:void(0)' onclick=\"remove('".$name."')\">Remove</a></div></td></tr>";
 								break;
 							}
 							$i++;
 						}
 					}
-					?>		
-					<tr><td><a class='link' href='javascript:void(0)' onclick="addadmin()">Add Administrator</a></td></tr>
-					<tr id='add' style='visibility:hidden'><td>Enter the username of the administrator to add:</td><td><input type='text' id='admintoadd'></td><td><a class='link' href='javascript:add()'>Add</a></td></tr>
-					<tr><td><input type='hidden' name='admin' id='admin' value=''/></td></tr>
+					?>
+					<tr class='table'><td><a class='link' href='javascript:void(0)' onclick="addadmin()">Add Administrator</a></td><td></td><td></td></tr>
+					<tr id='add' style='visibility:collapse'><td>Enter the username of the administrator to add:</td><td><input type='text' id='admintoadd'></td><td><div align='right'><a class='link' href='javascript:add()'>Add</a></div></td></tr>
+					<tr class='table' id='empty' style='visibility:hidden'><td><input type='hidden' name='admin' id='admin' value=''/></td><td></td><td></td></tr>
 					<tr><td><input type='hidden' name='remove' id='remove' value='false'/></td></tr>
 				</table>	
     </form>
