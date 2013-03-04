cf34eb99e95e086b8d253877ed791e2927034e8c
Rodrigo Santos <rigotek@gmail.com>
2013-03-04 10:50:14 -0800

    added stuff to anitas

--- MatchServe/application/views/CreateOrg.php
+++ MatchServe/application/views/CreateOrg.php
@@ -15,33 +15,50 @@
 </head>
 
 <body>
-	<form id="projectCreationForm" action=<?php echo URL::to('createorg/checkSubmit'); ?> method="get">
-	<table border = "0">
-	<tr>
-        <td> <input id="name" type="text" name="name" value = "Organization Name" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)" class="inputBox"/> </td>
-       </tr>
-     <tr> <td> <input id="address" type="text" name="address" value = "Address" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"> </td> </tr>
-     <tr>
-        <td> <input id="zipcode" type="text" name="zipcode" value = "Zip Code" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/> </td>
-       </tr>
-       <tr>  <td> <input id="phone" type="text" name="phone" value = "Phone" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/>  </td> </tr>
-       <tr>  <td> <input id="website" type="text" name="website" value = "Website" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/> </td> </tr>
-       <tr>  <td> <textarea id="mission" type="text" name="mission"  value = "Mission" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"   class="inputBox">Mission </textarea> </td> </tr>
-        </table>
+  <div class="header">
+    <?php echo render('elements.header'); ?>
+  </div>
 
-        
-       <div id="project-creation-causes-dropdown">
-			<div class="dropdown">
-               <a class="projectSkillSelector dropdown-toggle" data-toggle="dropdown" href="#"> Causes
-                    <span class="caret"></span>
-                </a>
-                <ul class = "dropdown-menu">
-                </ul>
-            </div>    
-    </div>
-    <br/> <br/>
-    <input type="submit" name="FinishButton" class="btn" value="Submit"/>
-      
-	</form>
+  <div class="dashboard">
+  </div>
+
+  <div class="subDashboard">
+  </div>
+
+  <div class="workspace">
+    <form id="projectCreationForm" action=<?php echo URL::to('createorg/checkSubmit'); ?> method="get">
+     <table border = "0">
+       <tr>
+        <td> <input id="name" type="text" name="name" value = "Organization Name" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)" class="inputBox"/> </td>
+      </tr>
+      <tr> <td> <input id="address" type="text" name="address" value = "Address" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"> </td> </tr>
+      <tr>
+        <td> <input id="zipcode" type="text" name="zipcode" value = "Zip Code" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/> </td>
+      </tr>
+      <tr>  <td> <input id="phone" type="text" name="phone" value = "Phone" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/>  </td> </tr>
+      <tr>  <td> <input id="website" type="text" name="website" value = "Website" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"  class="inputBox"/> </td> </tr>
+      <tr>  <td> <textarea id="mission" type="text" name="mission"  value = "Mission" onclick="fieldDisplay(this)" onfocus="focusedText(this)" onblur="blurText(this)"   class="inputBox">Mission </textarea> </td> </tr>
+    </table>
+
+
+    <div id="project-creation-causes-dropdown">
+     <div class="dropdown">
+       <a class="projectSkillSelector dropdown-toggle" data-toggle="dropdown" href="#"> Causes
+        <span class="caret"></span>
+      </a>
+      <ul class = "dropdown-menu">
+      </ul>
+    </div>    
+  </div>
+  <br/> <br/>
+  <input type="submit" name="FinishButton" class="btn" value="Submit"/>
+
+</form>
+</div>
+
+<div class="footer">
+  <?php echo render('elements.footer'); ?>
+</div>
+
 </body>
 </html>
