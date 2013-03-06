704d3058a267506f34066f168387a2978e71d5ee
Pierre Tasci <tasci@usc.edu>
2013-03-05 21:26:11 -0800

    Revert "basically got everything to display and tried to hook it up to the databse"
    This reverts commit 430e904be25823bdbb5bd246a95945627267d341.

--- MatchServe/application/controllers/dashboardvol.php
+++ MatchServe/application/controllers/dashboardvol.php
@@ -7,53 +7,5 @@ class DashboardVol_Controller extends Base_Controller{
 		//if user selects organization account, display organization account instead
 		return View::make('dashboardvol');
 	}
-	
-	public function action_initoptions(){
-		//query database and return a list of skills/causes
-		$table = $_GET['table'];
-		if ($table === "skills"){
-			 $data = Database::getSkills();
-			 $data = json_encode($data);
-			 return $data;
-		}else if($table === "causes"){
-			 $data = Database::getCauses();
-			 $data = json_encode($data);
-			 return $data;
-		}else{
-			echo "ERROR";
-			//error
-		}
-	}
-	
-	public function action_getprojects(){
-		$arguments = array();
-		$searchterm = null;
-		$zipcode = null;
-		//assumes number for zipcode
-		 // if( isset($_GET['zipcode']) && ("zip code" != $_GET['zipcode']) ) {
-		 //  	$zipcode = $_GET['zipcode'];
-		 // }
-			 if( isset($_GET['distance']) ){
-			 	$distance = $_GET['distance'];
-			 	$arguments['Location'] = $distance;
-			 }
-			 if( isset($_GET['skill']) ){
-			 	$arguments['Skills'] = $_GET['skill'];
-			 }
-			  // dd( isset($_GET['cause']) );
-			 if( isset($_GET['cause']) ){
-
-	 			$arguments['Cause'] = $_GET['cause'];
-
-			 }
-			 if( isset($_GET['time']) ){
-				$arguments['Time'] = $_GET['time'];
-			 }
-			  // dd($arguments);
-		 //DATABASE CALL that goes to models/Database.php
-		 $data = Database::getProjects($searchterm, $zipcode, $arguments);
-		 $data = json_encode($data);
-		 return $data;	
-	}
 }
 ?>
