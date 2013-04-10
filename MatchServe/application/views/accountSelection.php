0130da700cdac45097c9d5abd4c9ed9314fa29f7
Rodrigo Santos <rigotek@gmail.com>
2013-04-09 18:35:58 -0700

    changed the frontend of accountselection

--- MatchServe/application/views/accountSelection.php
+++ MatchServe/application/views/accountSelection.php
@@ -1,92 +1,77 @@
 <html>
 <head>
-<?php echo Asset::scripts();?>
+	<?php echo Asset::scripts();?>
 	<style type='text/css'>
-		body 
-		{ 
-			font-family:Arial;
-		}
-		.button 
-		{
-			background-color:rgb(204,204,204);color:rgb(119,119,119);padding:25px;padding-right:60px;padding-left:60px;
-			cursor: pointer;
-		}
-		.triangle-obtuse {
-	position:relative;
-	padding:15px;
-	margin:1em 0 3em;
-	color:#fff;
-	background:#777777;
-	/* css3 */
-	background:-webkit-gradient(linear, 0 0, 0 100%, from(#A8A8A8), to(#777777));
-	background:-moz-linear-gradient(#A8A8A8, #777777);
-	background:-o-linear-gradient(#A8A8A8, #777777);
-	background:linear-gradient(#A8A8A8, #777777);
-	-webkit-border-radius:10px;
-	-moz-border-radius:10px;
-	border-radius:10px;
-}
-
-/* creates the wider right-angled triangle */
-.triangle-obtuse:before {
-	content:"";
-	position:absolute;
-	bottom:-40px; /* value = - border-top-width - border-bottom-width */
-	left:80px; /* controls horizontal position */
-	border:0;
-	border-right-width:30px; /* vary this value to change the angle of the vertex */
-	border-bottom-width:40px; /* vary this value to change the height of the triangle. must be equal to the corresponding value in :after */
-	border-style:solid;
-	border-color:transparent #777777;
-    /* reduce the damage in FF3.0 */
-    display:block; 
-    width:0;
-}
+	body 
+	{ 
+		font-family:Arial;
+	}
+	.button 
+	{
+		padding:25px;
+		padding-right:60px;
+		padding-left:60px;
+		cursor: pointer;
+		background-color: #777777;
+		color:white;
+		margin:0 auto;
+	}
+	.dashboard p{
+		font-size: 30px;
+		color:white;
+		text-align: center;
+		margin:25px 80px;
+		line-height: 30px;
+	}
+	.subDashboard p{
+		text-align: center;
+		margin-top:10px;
+	}
+	table{
+		margin: 0 auto;
+	}
 	</style>
-	</head>
-	<body>
+</head>
+<body>
 	<div class="header">
-    <?php echo render('elements.header'); ?>
-  </div>
-    <div class="dashboard">
+		<?php echo render('elements.header'); ?>
 	</div>
-	  <div class="subDashboard">
-  </div>
-  <div class="workspace">
-	<table cellspacing='10px'>
-		<tr><td><div class="triangle-obtuse" style='-moz-border-radius:60px;border-radius:60px;width:300px;font-size:30px;color:white;background-color:rgb(119,119,119);padding:25px;font-style:italic;'>We found multiple accounts under your name. Which one would you like to use?</br>
-		<div style='font-size:15px;padding-top:10px;'>You can change these accounts later on. Just pick one for now.</div></div>
-		</td><td></td><td></td><td>
-		<form id='myform' action=<?php echo URL::to('user/accountselected'); ?> method='get'>
-			<table cellspacing='10px'>
-				<tr><td class='button' onclick="clicked('personal')" align='center'>PERSONAL ACCOUNT</td></tr>
-				<?php 
-				$userName = Session::get('userName');
-				$names = Session::get('names');
-				$names = json_decode($names);
-				if (is_array($names))
+	<div class="dashboard"><p>Please select an account to continue</p></div>
+	<div class="subDashboard"><p>You can change these accounts later on. Just pick one for now</p></div>
+	<div class="workspace">
+	</br>
+	</br>
+		<table cellspacing='10px'>
+			<form id='myform' action=<?php echo URL::to('user/accountselected'); ?> method='get'>
+				<table cellspacing='10px'>
+					<tr><td class='button' onclick="clicked('personal')" align='center'>My Personal Account</td></tr>
+					<?php 
+					$userName = Session::get('userName');
+					$names = Session::get('names');
+					$names = json_decode($names);
+					if (is_array($names))
 					{
-				foreach($names as $name)
-				{
-					echo "<tr><td class='button' onclick=\"clicked('$name->name')\" align='center'>$name->name</td></tr>";
-				}   
-				}
-				?>
-				<tr><td><input type='hidden' name='account' id='account' value=''/></td></tr>
-				<tr><td><input type='hidden' name='userName' value='$userName'/></td></tr>
-			</table>
-		</form>
-	</td></tr></table>
-	<script type='text/javascript'>
+						foreach($names as $name)
+						{
+							echo "<tr><td class='button' onclick=\"clicked('$name->name')\" align='center'>$name->name</td></tr>";
+						}   
+					}
+					?>
+					<tr><td><input type='hidden' name='account' id='account' value=''/></td></tr>
+					<tr><td><input type='hidden' name='userName' value='$userName'/></td></tr>
+				</table>
+			</form>
+		</td></tr></table>
+		<script type='text/javascript'>
 		function clicked(name) {
 			document.getElementById('account').value = name;
 			document.getElementById('myform').submit();
 		}
-	</script>
+		</script>
 	</div>
 	<div class="footer">
-  <?php echo render('elements.footer'); ?>
-</div>
+		<?php echo render('elements.footer'); ?>
+	</div>
 
-		</body>
+</body>
 </html>
